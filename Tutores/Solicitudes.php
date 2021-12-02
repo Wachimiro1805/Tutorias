b990db662d3b432af98c921618eaf10c74b1dedd
waryplumblum <edi_wachimiro1805@hotmail.com>
2021-12-02 02:01:19 -0700

    Update Solicitudes.php

--- Tutores/Solicitudes.php
+++ Tutores/Solicitudes.php
@@ -1,5 +1,6 @@
 <?php 
     session_start();
+    
 ?>
 <?php
 require "conexionT.php";
@@ -12,7 +13,11 @@ if($conexion->connect_errno)
     echo "Error de conexion de la base datos".$conexion->connect_error;
     exit();
 }
+
 $sql = "SELECT id_docente, nombre_docente, apellido_p, apellido_m FROM docentes;";
+
+
+
 
 $sql3 = "SELECT a.nombreA,a.apellido_p,a.apellido_m,a.numero_control,a.correo
           FROM alumnos a
@@ -22,9 +27,16 @@ $sql3 = "SELECT a.nombreA,a.apellido_p,a.apellido_m,a.numero_control,a.correo
 
 
 
-$sql2 = "SELECT D.id_docente, ATR.pk_solicitudes,A.nombreA, A.apellido_p,A.apellido_m, ATR.status,ATR.fecha,ATR.motivo 
+$sql2 = "SELECT D.id_docente, ATR.pk_solicitudes,A.nombreA, A.apellido_p,A.apellido_m, ATR.status,ATR.fecha,ATR.motivo,m.mensaje
           FROM alumnos A INNER JOIN solicitudes ATR ON (ATR.fk_alumnos =A.id_alumnos) 
-            INNER JOIN docentes D ON(D.id_docente = ATR.fk_docentes) WHERE D.usuario = '".$_docente."';";
+           INNER JOIN docentes D ON(D.id_docente = ATR.fk_docentes)
+           LEFT JOIN mensaje m ON (m.fk_docente=D.id_docente) WHERE D.usuario = '".$_docente."';";
+
+
+
+$id_docente = $conexion->query("SELECT id_docente from docentes where usuario = '".$_SESSION['control']."'");
+                    
+
 
 $resultado = $conexion->query($sql);
 $resultado2 = $conexion->query($sql2);
@@ -83,21 +95,89 @@ echo"Error: $error ";
                 <td style="border: 1px solid #000;">Status</td>
                 <td style="border: 1px solid #000;">Fecha</td>
                 <td style="border: 1px solid #000;">Observación y/o Motivo</td>
+                <td style="border: 1px solid #000;">Respuesta</td>
             </tr>
 
             <?php while($datos=$resultado2->fetch_array()){?>
             <tr>
-                <td><?php echo $datos["pk_solicitudes"]?></td>
-                <td><?php echo $datos["nombreA"]?></td>
-                <td><?php echo $datos["apellido_p"]?></td>
-                <td><?php echo $datos["apellido_m"]?></td>
-                <td><?php echo $datos["status"]?></td>
-                <td><?php echo $datos["fecha"]?></td>
-                <td><?php echo $datos["motivo"]?></td>
+                <td style="border: 1px solid #000; padding: 10px;"><?php echo $datos["pk_solicitudes"]?></td>
+                <td style="border: 1px solid #000; padding: 10px;"><?php echo $datos["nombreA"]?></td>
+                <td style="border: 1px solid #000; padding: 10px;"><?php echo $datos["apellido_p"]?></td>
+                <td style="border: 1px solid #000; padding: 10px;"><?php echo $datos["apellido_m"]?></td>
+                <td style="border: 1px solid #000; padding: 10px;"><?php echo $datos["status"]?></td>
+                <td style="border: 1px solid #000; padding: 10px;"><?php echo $datos["fecha"]?></td>
+                <td style="border: 1px solid #000; padding: 10px;"><?php echo $datos["motivo"]?></td>
+                <td style="border: 1px solid #000; padding: 10px;"><?php echo $datos["mensaje"]?></td>
+
             </tr>
             <?php } ?>
 
         </table>
+
+
+        <h1 align="center">Responder Solicitud</h1>
+        <form action="" method='post' align="center">
+            <?php
+                if (isset($_POST['btnSolicitar'])){
+                    include 'conexionT.php';
+
+                    $id_docente = $conexion->query("SELECT id_docente from docentes where usuario = '".$_SESSION['control']."'");
+                    echo"id_docente";
+
+                    $fecha_actual = date('Y-m-d');
+                    
+                    $motivo = $conexion->real_escape_string($_POST['motivo']);
+                    $asesoriaE =  $conexion->real_escape_string($_POST['alumnos']);
+                    $id_alumno = $conexion->query("SELECT id_alumnos from alumnos where numero_control = '".$_SESSION['control']."'");
+                    $rowAl = $id_alumno->fetch_array();
+                    $id_asesoria = $conexion->query("SELECT id_asesorias from asesorias where nombre = '$asesoriaE'");
+                    $rowAs = $id_asesoria->fetch_array();
+                    $idal = $rowAl['id_alumnos'];
+                    $idas = $rowAs['id_asesorias'];
+                    
+                    $fk_docent = $conexion->query("SELECT fk_docentes from asignar_tutor where fk_alumno = '$idal'");
+                    $rowAx = $fk_docent->fetch_array();
+                    if (isset($rowAx['fk_docentes'])){
+                    $idax = $rowAx['fk_docentes'];
+                    } else { return 'No se te ha asignado un tutor';}
+                        $insercion = $conexion->query("INSERT into mensaje (mensaje,fecha,fk_alumno,fk_docente) Values ('Solicitada', '$fecha_actual', '$motivo', '$idal', $idax,'$idas')");
+                            if ($insercion) {echo "Se ha hecho la solicitud";}
+                                else {
+                                    echo "No se ha podido responder la solicitud";
+                                }
+                }
+            ?>
+            
+            <?php
+                include 'conexionT.php';
+                $consulta = "SELECT A.nombreA,A.id_alumnos
+                FROM alumnos A INNER JOIN solicitudes ATR ON (ATR.fk_alumnos =A.id_alumnos) 
+                  INNER JOIN docentes D ON(D.id_docente = ATR.fk_docentes) WHERE D.usuario = '".$_docente."';";
+
+                $resultado = mysqli_query( $conexion, $consulta ) or die ( "Algo ha ido mal en la consulta a la base de datos");
+                echo "<select required name='alumnos'>";
+                while ($columna = mysqli_fetch_array( $resultado ))
+                {
+                    
+                    echo "<option value='". $columna['nombreA']."'>";
+                    echo $columna['nombreA'];
+                    echo "</option>";      
+                }
+                echo "<select>";
+                mysqli_close( $conexion );
+            ?>
+            <br></br>
+
+            <textarea name="mensaje" rows="2" cols="50" placeholder="Responde la solicitud." require></textarea>
+
+            <br></br>
+            <div class="buton">
+                <button onclick="location.href='Alumno.php?numero=16401013&tipo=asesoria'"
+                    name="btnSolicitar">SOLICITAR</button>
+
+            </div>
+        </form>
+
 
     </main>
 
@@ -117,7 +197,9 @@ echo"Error: $error ";
 
 
 
-
+<?php while($datos=$id_docente->fetch_array()){?>
+    <?php echo $datos["id_docente"]?>
+<?php } ?>
 
 
 </body>
